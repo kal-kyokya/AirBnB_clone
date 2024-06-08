0c58faae4696a0375e2ff1d97cc403971010f8a7
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-08 15:05:53 -0700

    Implement the Base model Cmass.

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+'base_model' is a class creation module
+"""
+import uuid
+from datetime import datetime
+
+
+def BaseModel():
+    """Defines all common attributes/methods of subclasses to come."""
+
+    def __init__(self):
+        """Constructs/Initializes Instances of class BaseModel."""
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Customizes the string representations of BaseModel instances"""
+        return f"[{slef.__class__.name}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Passes time at which method is called to 'updated_at'"""
+        self.updated_at = datetime.now
+
+    def to_dict(self):
+        """Generates dictionary representation of the BaseModel Instance."""
+        dct = self.__dict__.copy()
+        dct['created_at'] = self.created_at.isoformat()
+        dct['updated_at'] = self.updated_at.isoformat()
+        dct.update({'__class__': self.__class__.__name__})
+        return dct
