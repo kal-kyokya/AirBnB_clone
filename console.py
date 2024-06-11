c97c82c3be9f29a450020223c155803d62a3a1e4
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-11 16:41:56 -0700

    Create a command line interpretring session.

--- console.py
+++ console.py
@@ -1,7 +1,41 @@
 #!/usr/bin/python3
 """
-'pycodestyle' creates a simple python code conforming to pycodestyle.
+'console' is the entry point to the AirBnB console
 """
+import cmd
 
 
-print("Hello World\nThe syntax is correct.")
+class HBNBCommand(cmd.Cmd):
+    """Collection of attributes and methods enabling command interpretation.
+
+    Parent Class:
+        Cmd: Provides class fields for command manipulation.
+    """
+
+    prompt = "(hbnb) "
+    intro = "\nCurrently running: AirBnb_clone Command interpreter.\n"
+
+    def do_quit(self, line):
+        """Executes for input command 'quit'."""
+        return (True)
+
+    def help_quit(self):
+        """Documentation for help input command linked to auit command."""
+        print("When called, will exit/terminate the program.")
+
+    def do_EOF(self, line):
+        """Executes to exit cmdloop()."""
+        print()
+        return (True)
+
+    def help_EOF(self):
+        """Documentation pf End Of File signals as input (i.e. CTRL-D)"""
+        print("Handles 'CTRL-D' as input command and terminates execution.")
+
+    def empty_list(self):
+        """Executes when input command is 'an empty string'."""
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
