e79fbb518e6106498aaaffa3bb63600c0820532c
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-12 16:18:07 -0700

    Divide class attribute method set into individual tests.

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,12 +16,24 @@ from models.user import User
 class TestUser(unittest.TestCase):
     """Collection of methods testing parts of the 'user.py' script."""
 
-    def test_class_attributes(self):
-        """Asserts the presence of a set of class attributes."""
+    def test_email_class_attributes(self):
+        """Asserts the presence of the email class attributes."""
         obj = User()
         self.assertTrue(hasattr(obj, "email"))
+
+    def test_password_class_attributes(self):
+        """Asserts the presence of the password class attributes."""
+        obj = User()
         self.assertTrue(hasattr(obj, "password"))
+
+    def test_first_name_class_attributes(self):
+        """Asserts the presence of the first_name class attributes."""
+        obj = User()
         self.assertTrue(hasattr(obj, "first_name"))
+
+    def test_last_name_class_attributes(self):
+        """Asserts the presence of the last_name class attributes."""
+        obj = User()
         self.assertTrue(hasattr(obj, "last_name"))
 
 
