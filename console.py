b9e9e9663796faa8de84b0e1eb1d5c12b201d6dc
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-12 03:38:50 -0700

    Implement 'create' interpreter command.

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 'console' is the entry point to the AirBnB console
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -15,7 +17,11 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, line):
-        """Executes for input command 'quit'."""
+        """Executes for input command 'quit'.
+
+        Arg:
+            line: Potential input parameter to the 'quit' command.
+        """
         return (True)
 
     def help_quit(self):
@@ -23,7 +29,11 @@ class HBNBCommand(cmd.Cmd):
         print("When called, will exit/terminate the program.")
 
     def do_EOF(self, line):
-        """Executes to exit cmdloop()."""
+        """Executes to exit cmdloop().
+
+        Arg:
+            line: Potential input parameter to the 'quit' command.
+        """
         print()
         return (True)
 
@@ -34,6 +44,26 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Executes when input command is 'an empty string'."""
         pass
+
+    def do_create(self, cls_type=None):
+        """Initializes an object of specific class type.
+
+        Arg:
+            cls_type: Desired class type for object to be constructed.
+        """
+        if cls_type is None:
+            print("** class name missing **")
+        elif cls_type != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            obj = eval(cls_type)()
+            storage.new(obj)
+            storage.save()
+            print(f"{obj.id}")
+
+    def help_create(self):
+        """Documentation for the 'create' interpreter command."""
+        print("Constructs/Initializes an object of defined class type.")
 
 
 if __name__ == "__main__":
