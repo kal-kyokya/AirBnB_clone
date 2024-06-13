eb84e9eae06cdcd5667167dfee59b81ccc5ac6ca
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-12 17:26:42 -0700

    Test models.

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -11,9 +11,10 @@
 
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 
 
-class TestUser(unittest.TestCase):
+class TestUser1(unittest.TestCase):
     """Collection of methods testing parts of the 'user.py' script."""
 
     def test_email_class_attributes(self):
@@ -37,5 +38,24 @@ class TestUser(unittest.TestCase):
         self.assertTrue(hasattr(obj, "last_name"))
 
 
+class TestUser2(unittest.TestCase):
+    """Test cases against the User class"""
+
+    def test_attrs_are_class_attrs(self):
+        u = User()
+        # test that it is a class attribute
+        self.assertTrue(hasattr(User, "first_name")
+                        and hasattr(User, "last_name"))
+
+    def test_class_attrs(self):
+        u = User()
+        self.assertIs(type(u.first_name), str)
+        self.assertIs(type(u.last_name), str)
+        self.assertTrue(u.first_name == "")
+        self.assertTrue(u.last_name == "")
+
+    def test_user_is_a_subclass_of_basemodel(self):
+        u = User()
+        self.assertTrue(issubclass(type(u), BaseModel))
 if __name__ == "__main__":
     unittest.main()
